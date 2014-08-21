4b30b5595b4cf2307954e2ffa80342961a429c2d
sarahbethfederman <sarahbethfederman@gmail.com>
2014-08-21 13:01:19 -0400

    updates

--- calendar.js
+++ calendar.js
@@ -9,21 +9,25 @@
 var monthsList = ["August", "September", "October", "November", "December", "January", "March", "February", "April", "May", "June", "July"];
 
 var days = ["Sunday", "Monday","Tuesday","Wednesday","Thursday","Friday","Saturday"];
-var months = {};
+var months = {};  // associative array of month name to month object
 
+
+// Month object
 var Month = function(document) {
 
+  // Constructor
   function Month (name, startDay, dayCount) {
-    this.name = name;
-    this.startDay = startDay;
-    this.dayCount = dayCount;
+    this.name = name;               // name of the month
+    this.startDay = startDay;       // name of first day of the month (ex: August is Friday)
+    this.dayCount = dayCount;       // total number of days in the month
     this.populate();
   }
 
+  // Methods
+  // Create the text frames
   Month.prototype.populate = function() {
     var day = days.indexOf(this.startDay),
-        date,
-        weekStart;
+        date;
 
     for (var i = 1; i < this.dayCount + 1; i++) {
       date = i + ": " + days[day];
@@ -51,18 +55,21 @@ function findDayCount (month) {
 }
 
 function init() {
-  var dayStart = days.indexOf("Friday");  // August starts on Friday
-  var dayCount;
+  var startDayIndex = days.indexOf("Friday"),  // August starts on Friday, index of Friday in days array
+      dayCount,
+      monthName;
 
   for (var i = 0; i < monthsList.length; i++) {
-    month = monthsList[i];
+    monthName = monthsList[i];
     dayCount = findDayCount(month);
 
-    months[monthsList[i]] = new Month(month, days[dayStart], dayCount);
+    // Fill the months associative array
+    months[monthsList[i]] = new Month(monthName, days[startDayIndex], dayCount);
 
     console.log(months[monthsList[i]]);
 
-    dayStart = (dayStart + dayCount) % 7;
+    // Calculate next month's start day
+    startDayIndex = (startDayIndex + dayCount) % 7;
   }
 };
 
